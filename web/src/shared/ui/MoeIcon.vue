9e7b2a70e0158fef571008fb045b509e7ece4d18
TomyJan <TomyJan6@gmail.com>
2026-07-07 17:24:49 +0800

    refactor(图标): 显式处理太阳图标分支

--- web/src/shared/ui/MoeIcon.vue
+++ web/src/shared/ui/MoeIcon.vue
@@ -29,7 +29,7 @@
       <path d="M20 15.3A8.3 8.3 0 0 1 8.7 4a7 7 0 1 0 11.3 11.3Z" />
     </template>
 
-    <template v-else>
+    <template v-else-if="name === 'sun'">
       <circle cx="12" cy="12" r="4" />
       <path d="M12 2v2.2" />
       <path d="M12 19.8V22" />
@@ -39,6 +39,12 @@
       <path d="M19.8 12H22" />
       <path d="m4.9 19.1 1.6-1.6" />
       <path d="m17.5 6.5 1.6-1.6" />
+    </template>
+
+    <template v-else>
+      <circle cx="12" cy="12" r="7" />
+      <path d="M9.5 9a2.8 2.8 0 0 1 5 1.7c0 1.7-2.5 2.1-2.5 3.8" />
+      <path d="M12 17.5h.01" />
     </template>
   </svg>
 </template>
